75c68a345f06b338badff357096247af12041969
Zelionich <xedalx1@gmail.com>
2025-01-28 19:09:45 +0200

    программа больше или меньше 10

--- Program.cs
+++ Program.cs
@@ -1,4 +1,25 @@
-﻿Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, LALALALALAL!");
-Console.WriteLine("Hello, Зелеоник!!!!!!!!!!");
+﻿using System;
+class HelloWorld
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите число:");
+
+        // Преобразование строки в число с проверкой
+        if (int.TryParse(Console.ReadLine(), out int name))
+        {
+            if (name < 10)
+            {
+                Console.WriteLine("Число " + name + " меньше 10");
+            }
+            else
+            {
+                Console.WriteLine("Число " + name + " больше 10");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Введено не число!");
+        }
+    }
+}
